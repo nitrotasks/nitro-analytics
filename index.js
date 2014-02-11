938931efd4dc1957995556b06c2a2af73cbe181f
George Czabania <george@czabania.com>
2014-02-12 11:17:57 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+var ua, url, redis, visitor, visitors, rtg, GA_ID, http;
+
+// HTTP server
+http = require('http');
+
+// Google Analytics
+ua = require('universal-analytics');
+GA_ID = 'UA-47963532-1';
+
+// Connect to redis
+url = require('url');
+rtg = url.parse(process.env.REDIS_URL);
+redis = require('redis').createClient(rtg.port, rtg.hostname);
+redis.auth(rtg.auth.split(':')[1]);
+
+// Store visitors
+visitors = {};
+
+// Cache visitors
+visitor = function (id) {
+  return visitors.hasOwnProperty(id) ?
+    visitors[id] :
+    visitors[id] = ua(GA_ID);
+};
+
+redis.on('subscribe', function (channel, count) {
+  console.log('Listening to events on ' + channel);
+});
+
+redis.on('message', function (channel, message) {
+  var id, category, event;
+
+  console.log('message', channel, message);
+
+  id = message.match(/^(\d+)\|/);
+  id = id ? id[1] : undefined;
+
+  message = message.match(/\|?(\w+)\.(\w+)$/);
+  category = message[1];
+  event = message[2];
+
+  console.log({
+    id: id,
+    category: category,
+    event: event
+  });
+
+  visitor(id).event(category, event, id).send();
+
+});
+
+redis.subscribe('analytics');
+
+// Keep heroku happy
+http.createServer(function (req, res) {
+  res.end('hello world');
+}).listen(process.env.PORT || 8000);
+
